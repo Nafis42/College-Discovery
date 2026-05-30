d6acc06899f357edfd7f96fb3af683eb2e03c355
nafisdmk <157614573+nafisdmk@users.noreply.github.com>
2026-05-30 17:03:27 +0530

    fix

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 const result = dotenv.config();
 
-console.log(result);
+// console.log(result);
 
 import app from "./app.js";
 
@@ -9,5 +9,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-  console.log("PORT =", process.env.PORT);
+  // console.log("PORT =", process.env.PORT);
 });
